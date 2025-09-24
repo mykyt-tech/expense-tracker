4e646866532993e885e132979fff399ec4581a85
mykyt-tech <mykyt-tech@users.noreply.github.com>
2025-09-24 15:04:04 +0200

    feat: add calculateSummary method to calculate and get summary

--- src/TransactionManager.java
+++ src/TransactionManager.java
@@ -12,4 +12,30 @@ public class TransactionManager {
     public static ArrayList<Transaction> getTransactionHistory() {
         return new ArrayList<>(transactions);
     }
+
+    public static Summary calculateSummary() {
+        double totalIncome = 0;
+        double totalExpenses = 0;
+        double netBalance = 0;
+
+        for (Transaction t : transactions) {
+            TransactionType type = t.getType();
+            switch (type) {
+                case INCOME:
+                    totalIncome += t.getAmount();
+                    break;
+                case EXPENSE:
+                    totalExpenses += t.getAmount();
+                    break;
+                default:
+                    System.out.println("-----------------------------");
+                    System.out.println("Something went wrong");
+                    System.out.println("Summary cannot be calculated");
+                    return null;
+            }
+        }
+
+        netBalance = totalIncome - totalExpenses;
+        return new Summary(totalIncome, totalExpenses, netBalance);
+    }
 }
