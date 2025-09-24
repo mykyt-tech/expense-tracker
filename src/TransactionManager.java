80bea0bc1d059edd4718d4fecb54810c578b4fb6
mykyt-tech <mykyt-tech@users.noreply.github.com>
2025-09-24 14:41:16 +0200

    feat: add addNewTransaction method

--- src/TransactionManager.java
+++ src/TransactionManager.java
@@ -1,5 +1,12 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
 public class TransactionManager {
     private static ArrayList<Transaction> transactions = new ArrayList<>();
+
+    public static void addNewTransaction(String description, double amount, TransactionType type) {
+        LocalDate date = LocalDate.now();
+        transactions.add(new Transaction(date, description, amount, type));
+    }
 }
