b4a8017975412865599e174933b7539d5295ba34
mykyt-tech <mykyt-tech@users.noreply.github.com>
2025-09-25 12:56:18 +0200

    feat: add overloaded addNewTransaction method

--- src/TransactionManager.java
+++ src/TransactionManager.java
@@ -9,6 +9,10 @@ public class TransactionManager {
         transactions.add(new Transaction(date, description, amount, type));
     }
 
+    public static void addNewTransaction(LocalDate date, String description, double amount, TransactionType type) {
+        transactions.add(new Transaction(date, description, amount, type));
+    }
+
     public static ArrayList<Transaction> getTransactionHistory() {
         return new ArrayList<>(transactions);
     }
@@ -16,7 +20,7 @@ public class TransactionManager {
     public static Summary calculateSummary() {
         double totalIncome = 0;
         double totalExpenses = 0;
-        double netBalance = 0;
+        double netBalance;
 
         for (Transaction t : transactions) {
             TransactionType type = t.getType();
