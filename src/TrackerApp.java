febf4c78caa98d0bbd2425dd2f5a310981eab084
mykyt-tech <mykyt-tech@users.noreply.github.com>
2025-09-25 13:28:48 +0200

    feat: add methods to load and save transactions from/to file

--- src/TrackerApp.java
+++ src/TrackerApp.java
@@ -1,3 +1,5 @@
+import java.io.*;
+import java.time.LocalDate;
 import java.util.Scanner;
 
 public class TrackerApp {
@@ -6,5 +8,37 @@ public class TrackerApp {
         boolean running = true;
 
     }
-}
 
+    private static void writeTransactionsToFile(String fileName) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(fileName))) {
+            for (Transaction t : TransactionManager.getTransactionHistory()) {
+                bw.write(t.toString());
+                bw.newLine();
+            }
+        } catch (IOException e) {
+            System.out.println("-----------------------------");
+            System.out.println("Something went wrong");
+            System.out.println("Transactions cannot be saved to file");
+        }
+    }
+
+    private static void readTransactionsFromFile(String fileName) {
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] fields = line.split(",");
+                LocalDate date = LocalDate.parse(fields[0]);
+                String description = fields[1];
+                double amount = Double.parseDouble(fields[2]);
+                TransactionType type = TransactionType.valueOf(fields[3]);
+                TransactionManager.addNewTransaction(date, description, amount, type);
+            }
+        } catch (FileNotFoundException e) {
+            // Do nothing
+        } catch (IOException e) {
+            System.out.println("-----------------------------");
+            System.out.println("Something went wrong");
+            System.out.println("Transactions cannot be loaded from file");
+        }
+    }
+}
